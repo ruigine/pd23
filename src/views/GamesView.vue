0ee2d229ebad8802f94ad64855aecd8e3eb09126
rui <regine.sioh.2021@scis.smu.edu.sg>
2022-12-06 18:38:41 +0800

    sample

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -4,12 +4,30 @@
     </div>
 </template>
 <script>
+    // import 'firebase/compat/auth';
+    // import 'firebase/compat/firestore';
+    // import { db } from "../firebase/firebaseinit";
+    // import { collection, getDocs } from "firebase/firestore";
+
     export default {
         data(){
             return {
     
             }
         },
+        // created() {
+        //     getDocs(collection(db, 'vouchers'))
+        //     .then((snapshot) => {
+        //         var v = [];
+        //         snapshot.docs.forEach((doc) => {
+        //             v.push({ ...doc.data(), id: doc.id })
+        //         })
+        //         console.log(v);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     })
+        // }
     };
 </script>
 
