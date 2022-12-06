075875a91e706ac4d6563f2b5d6c25a378e1834f
MikeyTheOng <michaelongningofficial@gmail.com>
2022-12-06 17:17:47 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
   <v-app>
     <!-- Fixed Rendering -->
-<<<<<<< HEAD
-    <Navbar />
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-      >
-        <!--  -->
-      </v-navigation-drawer>
-=======
-
->>>>>>> master
 
       <v-app-bar app>
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -20,19 +9,12 @@
       </v-app-bar>
     <!-- Dynamic Rendering -->
     <v-main>
-      <router-view />
       Hello Word
     </v-main>
   </v-app>
 </template>
 
 <script>
-<<<<<<< HEAD
-// import { onBeforeMount } from "vue";
-// import { useStore } from "vuex";
-import Navbar from "./components/Navbar.vue";
-=======
->>>>>>> master
 
 export default {
   name: "App",
