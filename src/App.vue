1f05dfa7e9dc8a5bcebd64166499d9f8ceda5dad
MikeyTheOng <michaelongningofficial@gmail.com>
2022-12-06 17:40:44 +0800

    updated navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <template>
-  <v-app>
-    <!-- Fixed Rendering -->
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-      <v-app-bar app>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-divider></v-divider>
 
-        <v-toolbar-title>Application</v-toolbar-title>
-      </v-app-bar>
-    <!-- Dynamic Rendering -->
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
     <v-main>
-      Hello Word
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: "App",
-  components: {
-  },
-  data() {
-    return {
-      navigation: null,
-    };
-  },
-};
+  export default {
+    data(){
+      return{
+        drawer : false,
+        items: [
+          { title: 'Excel', icon: 'mdi-view-dashboard' },
+          { title: 'Tally', icon: 'mdi-help-box' },
+        ],
+      }
+    }
+  }
 </script>
-<style>
-
-</style>
