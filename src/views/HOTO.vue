63f6073da58801648abb860b03c1e9feac4ec64c
rui <regine.sioh.2021@scis.smu.edu.sg>
2022-12-06 17:06:14 +0800

    setup again

---
+++ src/views/HOTO.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        HOTO
+    </div>
+</template>
+<script>
+    import firebase from 'firebase/compat/app';
+    import 'firebase/compat/auth';
+    import 'firebase/compat/firestore';
+    import { auth, db } from "../firebase/firebaseinit";
+    import { getFirestore, doc, collection, query, where, onSnapshot, getDoc, getDocs, addDoc, setDoc, updateDoc, deleteDoc, orderBy } from "firebase/firestore";
+
+    export default {
+        data(){
+            return {
+    
+            }
+        },
+    };
+</script>
+
+<style lang="css" scoped>
+    @media only screen and (min-device-width: 411px) and (max-device-width: 823px) {
+    }
+    
+</style>
