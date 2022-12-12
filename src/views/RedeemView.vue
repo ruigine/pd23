77271d17759deacb6ac466e921a23cde14ce9bf1
Regine <regine.sioh.2021@smu.edu.sg>
2022-12-12 16:21:41 +0800

    vali

--- src/views/RedeemView.vue
+++ src/views/RedeemView.vue
@@ -72,7 +72,7 @@
                 sNo: '',
                 matricRules: [
                     m => !!m || 'Field is required',
-                    m => m.length == 8 || 'Matriculation number must be 8 digits long',
+                    m => (m && m.length == 8) || 'Matriculation number must be 8 digits long',
                     m => this.matricList.includes(m) == false || 'Matriculation number is already in database',
                 ],
                 sNoRules: [
