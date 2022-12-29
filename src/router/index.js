894c111a5ba319712edc036e8e1c46d3c612492c
Regine <regine.sioh.2021@smu.edu.sg>
2022-12-29 20:45:49 +0800

    luckydraw 1

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Games from "../views/GamesView.vue";
 import HOTO from "../views/HOTOView.vue";
+import LuckyDraw from "../views/LuckyDrawView.vue";
 import Login from "../views/LoginView.vue";
 import Redeem from "../views/RedeemView.vue";
 import Tally from "../views/TallyView.vue";
@@ -47,6 +48,12 @@ const routes = [
     path: "/hoto",
     name: "HOTO",
     component: HOTO,
+    beforeEnter: guardMyroute,
+  },
+  {
+    path: "/luckyDraw",
+    name: "Lucky Draw",
+    component: LuckyDraw,
     beforeEnter: guardMyroute,
   },
   {
