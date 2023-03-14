7e8fb47ddcdfe4404b3675d3fd45ef40341e2545
rui <regine.sioh.2021@scis.smu.edu.sg>
2023-03-14 17:20:51 +0800

    env

--- src/firebase/firebaseinit.js
+++ src/firebase/firebaseinit.js
@@ -3,19 +3,25 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+exports.handler = async () => {
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+  
+  // initialize firebase app
+  const app = initializeApp(firebaseConfig);
+
+  return {
+    statusCode: 200,
+  };
 };
 
-
-// initialize firebase app
-const app = initializeApp(firebaseConfig);
 
 // initialize services
 const db = getFirestore(app);
